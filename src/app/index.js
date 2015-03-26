7e84a1c11d9831c16fc46bada2d2ad3bf9748f29
Jeff DeButte <jeff@inharmonic.com>
2015-03-25 21:35:44 -0400

    Adding an angular scaffold w/ libsass, handlebars, foundation

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('icons', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mm.foundation'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('icons')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
