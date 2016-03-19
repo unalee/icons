08ad31c85974720ba5aafdfe946b9de406f71a1b
Jeff DeButte <jeff@inharmonic.com>
2016-03-19 12:11:16 -0400

    working icons list & detail

--- frontend/src/app/iconDetail.controller.js
+++ frontend/src/app/iconDetail.controller.js
@@ -1,14 +1,18 @@
 'use strict';
 
 angular.module('icons')
-  .controller('iconDetailCtrl', function ($scope, $state, $http) {
+  .controller('iconDetailCtrl', function ($scope, $rootScope, $state, $http) {
 
   	$scope.icons = [];
   	$scope.icon = {};
   	//$scope.loading = true;
     $http.get('/assets/json/icons.json').success(function(data) {
-    	console.log(data)
+    	
     	$scope.icons = data;
+    	if(angular.isDefined($state.params.iconId)) {
+    		$scope.icon = $scope.icons[parseInt($state.params.iconId)];
+    	}
+
     }).error(function() {
     	$rootScope.$broadcast('iconsDisplayMessage', {
     		type: "alert",
