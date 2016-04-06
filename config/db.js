e8f025e76be1261516d92a240c3fba42fb8cdd39
Alex Leitch <alex.leitch@gmail.com>
2016-04-05 20:46:52 -0400

    fix(local server): fixing db

--- config/db.js
+++ config/db.js
@@ -1,25 +1,38 @@
 // db.js
 'use strict';
 
-var mongoose = require('mongoose');
-var uriUtil = require('mongodb-uri');
-
-// var mongoUrl = 'mongodb://archive_admin:basca81240yh1ec@ds013738.mlab.com:13738/vision_archive';
-var mongoUrl = 'mongodb://127.0.0.1/vision_archive'
-
-var mongooseUri = uriUtil.formatMongoose(mongoUrl);
-
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
-
+const mongoose = require('mongoose'),
+  uriUtil = require('mongodb-uri'),
+  // var mongoUrl = 'mongodb://archive_admin:basca81240yh1ec@ds013738.mlab.com:13738/vision_archive';
+  mongoUrl = 'mongodb://127.0.0.1/vision_archive',
+  mongooseUri = uriUtil.formatMongoose(mongoUrl),
+  options = {
+    server: {
+      socketOptions: {
+        keepAlive: 1,
+        connectTimeoutMS: 30000
+      }
+    },
+    replset: {
+      socketOptions: {
+        keepAlive: 1,
+        connectTimeoutMS: 30000
+      }
+    }
+  };
 mongoose.connect(mongoUrl, options);
-var db = mongoose.connection;
+
+let db = mongoose.connection;
 
 console.log('opening db...');
-db.on('error', function(err) { console.log("local error opening the db: " + err.message); });
-db.once('open', function callback () {
-	// console.log(mongoUrl); // useful for checking where we're hooked to
-    console.log('db open');
+db.on('error', function(err) {
+  console.log("local error opening the db: " + err.message);
+});
+db.once('open', function callback() {
+  // console.log(mongoUrl); // useful for checking where we're hooked to
+  console.log('db open');
 });
 
-module.exports = { db: db };
+module.exports = {
+  db: db
+};
